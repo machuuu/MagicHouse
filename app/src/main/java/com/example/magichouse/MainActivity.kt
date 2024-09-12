00a19e77d800c7be28210c64a0b90dd93da8ddb9
Matthew M <21700872+machuuu@users.noreply.github.com>
2024-09-11 21:47:23 -0500

    Initial commit for OpenGL Context as Fullscreen

---
+++ app/src/main/java/com/example/magichouse/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.magichouse
+
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.magichouse.ui.theme.MagicHouseTheme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.viewinterop.AndroidView
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var gLView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+    }
+}
